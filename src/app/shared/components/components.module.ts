979e434b1545174b4c80870996b8ff5adfa5cf78
Neeraj <singh.neer@ymail.com>
2019-12-31 10:19:23 +0530

    ipx-Checkbox control completed

--- src/app/shared/components/components.module.ts
+++ src/app/shared/components/components.module.ts
@@ -7,17 +7,18 @@ import { ElementBaseComponent } from './element.base/element.base.component';
 import { TextFieldComponent } from './text-field/text-field.component';
 import { IpxDropdownComponent } from './custom-dropdown/ipx-dropdown.component';
 import { IpxRadioButtonComponent } from './radio-button/ipx-radio-button.component';
+import { IpxCheckboxComponent } from './check-box/check-box.component';
 
 
 
 @NgModule({
-  declarations: [ComboTextboxComponent, IpxDropdownComponent, ElementBaseComponent, TextFieldComponent, IpxRadioButtonComponent],
+  declarations: [ComboTextboxComponent, IpxDropdownComponent, ElementBaseComponent, TextFieldComponent, IpxRadioButtonComponent, IpxCheckboxComponent],
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule
   ],
-  exports: [ComboTextboxComponent, IpxDropdownComponent, ElementBaseComponent, TextFieldComponent, IpxRadioButtonComponent],
+  exports: [ComboTextboxComponent, IpxDropdownComponent, ElementBaseComponent, TextFieldComponent, IpxRadioButtonComponent, IpxCheckboxComponent],
   entryComponents: [ IpxDropdownComponent, TextFieldComponent]
 })
 export class ComponentsModule { }
